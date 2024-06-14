c90c8c57c96944d2f0985928aacf3b8f7884cb5b
bawangMekanik <sarwogarnaco@gmail.com>
2024-06-14 16:17:36 +0700

    Add files via upload

---
+++ js.js
@@ -0,0 +1,11 @@
+import Swal from 'sweetalert2'
+import Swal from 'sweetalert2/dist/sweetalert2.js'
+import 'sweetalert2/src/sweetalert2.scss'
+
+Swal.fire({
+    title: "The Internet?",
+    text: "That thing is still around?",
+    icon: "question"
+  });
+
+  alert("welcome");
