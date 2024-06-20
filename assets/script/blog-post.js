abd98c925b67beb76c057448c669df639cdd498c
MekanikBawang <sarwogarnaco@gmail.com>
2024-06-20 17:16:51 +0700

    Update blog-post.js

--- assets/script/blog-post.js
+++ assets/script/blog-post.js
@@ -1,6 +1,3 @@
-const menuToggle = document.querySelector('.menu-toggle');
-const navUl = document.querySelector('nav ul');
-
 // Event listener untuk klik tombol toggle
 menuToggle.addEventListener('click', function() {
     navUl.classList.toggle('open');
