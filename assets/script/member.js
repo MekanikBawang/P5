f4fd47238a5e2e040454860354f5d6d8117a8ec4
MekanikBawang <sarwogarnaco@gmail.com>
2024-06-20 17:16:36 +0700

    Update member.js

--- assets/script/member.js
+++ assets/script/member.js
@@ -18,9 +18,6 @@ function scrollFunction() {
     }
 }
 
-const menuToggle = document.querySelector('.menu-toggle');
-const navUl = document.querySelector('nav ul');
-
 // Event listener untuk klik tombol toggle
 menuToggle.addEventListener('click', function() {
     navUl.classList.toggle('open');
